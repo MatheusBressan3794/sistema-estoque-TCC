72e0862af709777a5b213272db4fd674cf4cc4d4
Rhuan Alvarinho <alvarinhorhuan@gmail.com>
2026-09-22 23:06:05 -0300

    fix: atualiza build.sh com collectstatic e migrate

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 # Exit on error
 set -o errexit
 
+# Instala as dependências
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Executa as migrações da base de dados
 python manage.py migrate
+
+# Recolhe os ficheiros estáticos para o WhiteNoise funcionar
+python manage.py collectstatic --no-input
