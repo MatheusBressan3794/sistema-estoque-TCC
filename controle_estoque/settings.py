774ccdf2d28f533ee626c9aa6234fe5e91b88e74
Rhuan Alvarinho <alvarinhorhuan@gmail.com>
2026-09-22 22:52:33 -0300

    fix: adiciona tratamento de limpeza na url da base de dados

--- controle_estoque/settings.py
+++ controle_estoque/settings.py
@@ -75,13 +75,19 @@ WSGI_APPLICATION = 'controle_estoque.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+db_url = os.environ.get('DATABASE_URL', '')
+if db_url:
+    db_url = db_url.strip('"\'')
+
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL'),
+        db_url,
         conn_max_age=600,
         ssl_require=False
     )
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
